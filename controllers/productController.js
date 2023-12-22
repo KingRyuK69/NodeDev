c0d0266be6205f402ca84812101f2a4e9338a114
KingPuke <154311503+KingPuke@users.noreply.github.com>
2023-12-22 18:38:57 +0530

    GetFile

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,105 +1,133 @@
-const Product = require('../models/productModel')
-const multer = require('multer')
+const Product = require("../models/productModel");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 //get all product
-const getProducts = async(req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products);
-      
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //get a single prod
-const getProduct = async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id)
-        res.status(200).json(product);
-      
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //create a new product
-const createProduct = async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product);
-      
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({message: error.message})
-    }
-}
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //update a product
-const updateProduct = async(req, res) => {
-    try {
-      const {id} = req.params;
-      const product = await Product.findByIdAndUpdate(id, req.body);
-      // we can't find the prod
-      if(!product){
-        return res.status(404).json({message: `Product can't be found with ID ${id}`})
-      }
-      const updatedProduct = await Product.findById(id);
-      res.status(200).json(updatedProduct);
-      
-    } catch (error) {
-      res.status(500).json({message: error.message});
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we can't find the prod
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `Product can't be found with ID ${id}` });
     }
-}
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //delete a product
-const deleteProduct = async(req, res) =>{
-    try {
-      const {id} = req.params;
-      const product = await Product.findByIdAndDelete(id);
-      // we can't find the prod
-      if(!product){
-        return res.status(404).json({message: `my product can't be found with ID ${id}`})
-      }
-      res.status(200).json(product);
-      
-    } catch (error) {
-      res.status(500).json({message: error.message});
-      
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    // we can't find the prod
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `my product can't be found with ID ${id}` });
     }
-}
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 //upload file
-const uploadFile = async(req, res) => {
+const uploadFile = async (req, res) => {
   try {
-      if(!req.file){
-          return res.status(400).json({message: "No file uploaded"})
-      }
-      res.status(200).json({message: "Single File Uploaded", file: req.file})
-
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+    res.status(200).json({ message: "Single File Uploaded", file: req.file });
   } catch (error) {
-      res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
   }
-}
+};
 
 const fileStorageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./images")
+    cb(null, "./images");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '--' + file.originalname)
-  }
+    cb(null, Date.now() + "--" + file.originalname);
+  },
 });
 
-const upload = multer({storage: fileStorageEngine});
+const upload = multer({
+  storage: fileStorageEngine,
+  fileFilter: (req, file, cb) => {
+    console.log(file.originalname);
+    let ext = path.extname(file.originalname);
+    console.log("ext", ext);
+    if (ext != ".png" && ext != ".jpg" && ext != ".jpeg") {
+      req.fileValidationError = "Forbidden extension";
+      return cb(null, false, req.fileValidationError);
+    }
+    cb(null, true);
+  },
+});
+
+//get a file
+const getFile = async (req, res) => {
+  try {
+    const filename = req.params.filename;
+    const filePath = path.join("images", filename);
+
+    if (fs.existsSync(filePath)) {
+      res.status(200).json({ message: "Single File Found", file: filename });
+    } else {
+      throw new Error("File not found");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
-    getProducts,
-    getProduct,
-    createProduct, 
-    updateProduct,
-    deleteProduct,
-    uploadFile,
-    upload 
-}
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadFile,
+  upload,
+  getFile,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,28 +1,39 @@
-const express = require('express');
-const Product = require('../models/productModel')
-const {getProducts, getProduct, createProduct, updateProduct, deleteProduct, uploadFile} = require('../controllers/productController')
+const express = require("express");
+const Product = require("../models/productModel");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  uploadFile,
+  getFile,
+} = require("../controllers/productController");
 
 //add prod validation middleware
-const {addUserValidation} = require('../validations/eValidate')
-const {upload} = require('../controllers/productController')
+const { addUserValidation } = require("../validations/eValidate");
+const { upload } = require("../controllers/productController");
 const router = express.Router();
 
+//get a file
+router.get("/get-file/:filename", getFile);
+
 //get all products
-router.get('/', getProducts);
+router.get("/", getProducts);
 
 //get a new product
-router.get('/:id', getProduct);
+router.get("/:id", getProduct);
 
 //create a new product
-router.post('/', addUserValidation, createProduct);
+router.post("/", addUserValidation, createProduct);
 
 // update a prod
-router.put('/:id', updateProduct);
-  
+router.put("/:id", updateProduct);
+
 //delete a prod
-router.delete('/:id', deleteProduct);
+router.delete("/:id", deleteProduct);
 
 //upload a file
-router.post('/single', upload.single("image"), uploadFile);
+router.post("/single", upload.single("image"), uploadFile);
 
 module.exports = router;
